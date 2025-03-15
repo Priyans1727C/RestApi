5cf9691e7d491906771d5e8797e7668b41293b75
Priyanshu Singh <sbhola833@gmail.com>
2025-03-15 08:25:08 +0530

    Extend CustomUserAdmin to include full_name and phone_number fields

--- api/authentication/admin.py
+++ api/authentication/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # Extend the existing fieldsets to include the new fields
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('full_name', 'phone_number')}),
+    )
+    # Include fields on the add user page as well
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('full_name', 'phone_number')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
