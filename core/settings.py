e9ff08134806d69e14d10a6f168b8d6a0a841be2
Priyanshu Singh <sbhola833@gmail.com>
2025-03-15 08:09:59 +0530

    Add root view and update URL routing; configure static files path

--- core/settings.py
+++ core/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR, "static"),  # Make sure this path exists
 ]
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import root
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', root, name='root'),
     path('api/auth/', include('api.authentication.urls')),
 ]
